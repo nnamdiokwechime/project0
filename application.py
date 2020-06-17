ea0f34a2a917503fde666552f86b16ea15dbef41
nnamd <nnamdiokwechime@yahoo.com>
2020-06-16 20:23:40 -0400

    added session function

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, session
+from flask_session import Session
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"]= False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
@@ -8,6 +12,6 @@ def index():
 
 @app.route("/hello", methods= ["POST"])
 def hello():
-    name= request.form.get("name").capitalize()
-    country = request.form.get("country") 
-    return f"Hello, {name} from {country}"
+    session["name"]= request.form.get("name").capitalize()
+    session["country"] = request.form.get("country")
+    return f"Hello, {session['name']} from {session['country']}"
